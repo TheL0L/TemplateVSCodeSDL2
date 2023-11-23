541a42882c804d5fba4192323b90f34a4e9b4ddb
Sergei <sergeiyakima1@gmail.com>
2023-11-23 21:47:23 +0200

    rewritten example game rendering and handling

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,16 @@
 #include "RenderWindow.hpp"
 #include "Entity.hpp"
 
-#include "fourGame.hpp"
-#include <vector>
+#include "GameState.hpp"
+#define CELL_SIZE 98
+#include <string>
 
 bool init();
-void boundEntity(Entity& entity, Vector2D& velocity, const Vector2D& windowSize);
-void boundEntity(Entity& entity, Vector2D& velocity, const SDL_FRect& boundingFrame);
 
 Vector2D screenToGrid(const RenderWindow& window, const Vector2D& clickPos);
+void drawGrid(RenderWindow& window);
+void drawPieces(RenderWindow& window, const GameState& gameState,
+    CustomTexture& circleRed, CustomTexture& circleBlack);
 
 int main(int argc, char** argv)
 {
@@ -24,15 +26,15 @@ int main(int argc, char** argv)
         return 0;
     }
 
-    RenderWindow window("4 in a Row  |  press ESC to reset board", 490, 490, false, 25);
+    RenderWindow window("Connect 4  |  press ESC to reset game",
+        CELL_SIZE * CONNECT4_COLS, CELL_SIZE * CONNECT4_ROWS,
+        false, 25
+    );
+    window.setDrawColor(CustomColor::NAVY);
 
     bool isRunning = true;
     SDL_Event event;
 
-    GameState gameState;
-
-    SDL_Texture* gameBoardTexture = window.loadTexture("./resources/board_49x49.png");
-    CustomTexture gameBoard(gameBoardTexture, CustomRect(0, 0, 49, 49));
 
     SDL_Texture* circleRedTexture = window.loadTexture("./resources/redCircle_49x49.png");
     CustomTexture circleRed(circleRedTexture, CustomRect(0, 0, 49, 49));
@@ -40,10 +42,12 @@ int main(int argc, char** argv)
     SDL_Texture* circleBlackTexture = window.loadTexture("./resources/blackCircle_49x49.png");
     CustomTexture circleBlack(circleBlackTexture, CustomRect(0, 0, 49, 49));
 
-    window.setDrawColor(CustomColor::NAVY);
 
+    GameState gameState;
     Vector2D clickPos;
-    bool clicked = false;
+    bool madeMove = false;
+    bool mouseDown = false;
+    bool escapeDown = false;
 
     while (isRunning)
     {
@@ -55,13 +59,37 @@ int main(int argc, char** argv)
                 isRunning = false;
                 break;
 
+            case SDL_EventType::SDL_MOUSEBUTTONDOWN:
+                if (event.button.button == SDL_BUTTON_LEFT && !mouseDown)
+                {
+                    mouseDown = true;
+                    clickPos = Vector2D(event.button.x, event.button.y);
+                    madeMove = true;
+                }
+                break;
+
             case SDL_EventType::SDL_MOUSEBUTTONUP:
-                clickPos = Vector2D(event.button.x, event.button.y);
-                clicked = true;
+                if (event.button.button == SDL_BUTTON_LEFT)
+                {
+                    mouseDown = false;
+                }
+                break;
+
+            case SDL_EventType::SDL_KEYDOWN:
+                if (event.key.keysym.sym == SDL_KeyCode::SDLK_ESCAPE && !escapeDown)
+                {
+                    escapeDown = true;
+                    gameState.reset();
+                    window.setTitle("Connect 4  |  press ESC to reset game");
+                    madeMove = false;
+                }
                 break;
 
             case SDL_EventType::SDL_KEYUP:
-                gameState.reset();
+                if (event.key.keysym.sym == SDL_KeyCode::SDLK_ESCAPE)
+                {
+                    escapeDown = false;
+                }
                 break;
 
             default:
@@ -71,38 +99,26 @@ int main(int argc, char** argv)
 
         window.clear();
 
-        window.draw(gameBoard.getTexture(), gameBoard.getSrcRect(), CustomFRect(0, 0, window.getSize().x, window.getSize().y));
-
-        if (clicked)
+        if (gameState.hasEnded())
         {
+            std::string player = gameState.getTurn() == Piece::RED ? "RED" : "BLACK";
+            std::string title = "Player " + player + " has won!  press ESC to reset game";
+            window.setTitle(title.c_str());
+        }
+
+        if (madeMove && gameState.hasEnded() == false)
+        {
+            madeMove = false;
             Vector2D gridPos = screenToGrid(window, clickPos);
 
             if (gameState.makeMove(gridPos.x) == false)
             {
                 std::cout << "bad move!" << std::endl;
             }
-            gameState._board.print();
-
-            clicked = false;
         }
 
-        auto board = gameState.getBoard();
-        for (int row = 6; row >= 0; --row)
-        {
-            for (int col = 0; col < 7; ++col)
-            {
-                Vector2D roundedPos(col * window.getSize().y / 7, window.getSize().y - (row + 1) * window.getSize().y / 7);
-                // RED=1 BLACK=2
-                if (board[col + row * 7] == Piece::RED)
-                {
-                    window.draw(circleRed.getTexture(), circleRed.getSrcRect(), CustomFRect(roundedPos.x, roundedPos.y, circleRed.getSrcRect().w, circleRed.getSrcRect().h));
-                }
-                else if (board[col + row * 7] == Piece::BLACK)
-                {
-                    window.draw(circleBlack.getTexture(), circleBlack.getSrcRect(), CustomFRect(roundedPos.x, roundedPos.y, circleBlack.getSrcRect().w, circleBlack.getSrcRect().h));
-                }
-            }
-        }
+        drawGrid(window);
+        drawPieces(window, gameState, circleRed, circleBlack);
 
         window.display();
         window.frameLimiter();
@@ -132,43 +148,80 @@ bool init()
     return true;
 }
 
-void boundEntity(Entity& entity, Vector2D& velocity, const Vector2D& windowSize)
-{
-    SDL_FRect boundingFrame;
-    boundingFrame.x = boundingFrame.y = 0;
-    boundingFrame.w = windowSize.x;
-    boundingFrame.h = windowSize.y;
-
-    boundEntity(entity, velocity, boundingFrame);
-}
-
-void boundEntity(Entity& entity, Vector2D& velocity, const SDL_FRect& boundingFrame)
-{
-    SDL_FRect entityBound = entity.getBoundingBox();
-
-    if (entityBound.x < boundingFrame.x)
-    {
-        velocity.x = abs(velocity.x);
-    }
-    else if (entityBound.w > boundingFrame.w)
-    {
-        velocity.x = -1 * abs(velocity.x);
-    }
-
-    if (entityBound.y < boundingFrame.y)
-    {
-        velocity.y = abs(velocity.y);
-    }
-    else if (entityBound.h > boundingFrame.h)
-    {
-        velocity.y = -1 * abs(velocity.y);
-    }
-}
-
 Vector2D screenToGrid(const RenderWindow& window, const Vector2D& clickPos)
 {
-    Vector2D winSize = window.getSize() / 7;
-    Vector2D gridCoord((int)clickPos.x / (int)winSize.x % 7, (int)clickPos.y / (int)winSize.y % 7);
+    Vector2D winSize = window.getSize();
 
-    return gridCoord;
+    // get the clickPos as [0,1] ratio of the window
+    float x = clickPos.x / winSize.x;
+    float y = clickPos.y / winSize.y;
+
+    // multiply by grid size and round down
+    x = (int)(x * CONNECT4_COLS);
+    y = (int)(y * CONNECT4_ROWS);
+
+    return Vector2D{ x, y };
+}
+
+void drawGrid(RenderWindow& window)
+{
+    Vector2D A, B;
+
+    // vertial lines
+    A.y = 0;
+    B.y = window.getSize().y;
+    for (int col = 1; col < CONNECT4_COLS; ++col)
+    {
+        A.x = col * CELL_SIZE;
+        B.x = col * CELL_SIZE;
+        window.drawLine(A, B, CustomColor::WHITE);
+    }
+
+    // horizontal lines
+    A.x = 0;
+    B.x = window.getSize().x;
+    for (int row = 1; row < CONNECT4_ROWS; ++row)
+    {
+        A.y = row * CELL_SIZE;
+        B.y = row * CELL_SIZE;
+        window.drawLine(A, B, CustomColor::WHITE);
+    }
+}
+
+void drawPieces(RenderWindow& window, const GameState& gameState,
+    CustomTexture& circleRed, CustomTexture& circleBlack)
+{
+    auto board = gameState.getBoard();
+    for (int col = 0; col < CONNECT4_COLS; ++col)
+    {
+        for (int row = 0; row < CONNECT4_ROWS; ++row)
+        {
+            if (board[col + row * CONNECT4_COLS] == Piece::NONE)
+            {
+                continue;
+            }
+            Vector2D pieceDrawPos(
+                col * CELL_SIZE,
+                (CONNECT4_ROWS - row - 1) * CELL_SIZE  // screen(0,0) is topleft, but player expects bottomleft
+            );
+            if (board[col + row * CONNECT4_COLS] == Piece::RED)
+            {
+                window.draw(circleRed.getTexture(), circleRed.getSrcRect(),
+                    CustomFRect(
+                        pieceDrawPos.x, pieceDrawPos.y,
+                        CELL_SIZE, CELL_SIZE
+                    )
+                );
+            }
+            else if (board[col + row * CONNECT4_COLS] == Piece::BLACK)
+            {
+                window.draw(circleBlack.getTexture(), circleBlack.getSrcRect(),
+                    CustomFRect(
+                        pieceDrawPos.x, pieceDrawPos.y,
+                        CELL_SIZE, CELL_SIZE
+                    )
+                );
+            }
+        }
+    }
 }
